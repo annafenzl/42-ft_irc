a1496c9f2b606b4a6ba403f7e75c4d6a5f78d12c
Pavel Guranda <pguranda@2-B-5.42heilbronn.de>
2023-04-19 18:12:52 +0200

    before merge

---
+++ obj/main.d
@@ -0,0 +1,14 @@
+obj/main.o: main.cpp inc/User.hpp inc/Server.hpp inc/response.hpp \
+  inc/Request.hpp inc/Channel.hpp inc/Bot.hpp
+
+inc/User.hpp:
+
+inc/Server.hpp:
+
+inc/response.hpp:
+
+inc/Request.hpp:
+
+inc/Channel.hpp:
+
+inc/Bot.hpp:
